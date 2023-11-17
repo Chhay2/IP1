5e38d5e9554f3c38cb2be1a2eaaa2ea0281e2ccf
Chhay2 <117246850+Chhay2@users.noreply.github.com>
2023-11-17 13:24:51 +0700

    re-build

--- src/main.js
+++ src/main.js
@@ -1,22 +1,11 @@
-<<<<<<< HEAD
-// import './assets/main.css'
-=======
->>>>>>> d1bbb475af2b158f4249130eee664525f4e2d688
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import router from './router'
 import App from './App.vue'
 import router from './router'
-
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router);
-<<<<<<< HEAD
-
-=======
->>>>>>> d1bbb475af2b158f4249130eee664525f4e2d688
 
 app.mount('#app')
